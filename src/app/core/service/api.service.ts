de9320cc4e0c3515327e74458b75d48c8ca9713e
tushthorat <thoratt49@gmail.com>
2024-04-04 21:26:28 +0530

    created Api services

--- src/app/core/service/api.service.ts
+++ src/app/core/service/api.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {  Observable, catchError,throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+  httpOptions={
+    headers:new HttpHeaders({
+      "Content-Type":"application/json",
+      "Access-Control-allow-Origin":"*"
+    })
+  }
+  constructor(private http:HttpClient) { }
+  private formatError(error:any){
+    return throwError(error.error);
+  }
+  get(path:string,params:HttpParams=new HttpParams()):Observable<any>{
+      return this.http.get(path,{params}).pipe(catchError(this.formatError))
+  }
+  put(path:string,body:Object={}):Observable<any>{
+       return this.http.put(path,JSON.stringify(body),this.httpOptions).pipe(catchError(this.formatError))
+  }
+  post(path:string,body:Object={}):Observable<any>{
+    return this.http.post(path,JSON.stringify(body),this.httpOptions).pipe(catchError(this.formatError))
+  }
+  delete(path:string){
+    return this.http.delete(path).pipe(catchError(this.formatError))
+  }
 }
