728ead4f348eef665974828145cbb3ad95fe6bed
tushthorat <thoratt49@gmail.com>
2024-04-09 14:45:05 +0530

    userwise header display logic implemented

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +9,32 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  
+    logged_in:boolean=false;
+    language:string="English";
+    user_role:any;
+     
+    constructor(private router:Router){
+      
+    }
+    ngOnInit(): void {
+      
+    }
+
+    //ngDoCheck automatically call everytime when we click anywhere on the page
+    ngDoCheck(){
+         let user_role=sessionStorage.getItem("role");
+         let user_session_id=sessionStorage.getItem("user_session_id");
+         if(user_session_id){
+            this.logged_in=true;
+         }
+    }
+    logout(){
+      sessionStorage.removeItem("user_session_id");
+      sessionStorage.removeItem("user_role");
+      this.router.navigateByUrl('/sign-in');
+      location.reload();
+    }
 
 }
