a47207b1354e9928dfe402df621614d0d3018c27
tushthorat <thoratt49@gmail.com>
2024-04-10 23:44:13 +0530

    implemented  checkout  component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layouts/header/header.component';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,FormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ECommerce_Angular';
+  screenHeight:any;
+  screenWidth:any;
+  footerMaxHeight!:number;
+  //title = 'angularecommerce';
+
+  constructor(){
+    this.getScreenSize(event);
+  }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event:any){
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    //console.log(this.screenHeight, this.screenWidth)
+    this.footerMaxHeight = this.screenHeight -160;
+  }
 }
--- src/app/customer/buyer/buyer-dashboard/buyer-dashboard.component.ts
+++ src/app/customer/buyer/buyer-dashboard/buyer-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-buyer-dashboard',
@@ -9,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class BuyerDashboardComponent {
 
+  all_products:any;
+  show_Checkout:boolean =false;
+  constructor(private router:Router, private customerService:CustomerService){}
+
+  ngOnInit(): void {
+ this.getAllProduct()
+  }
+  getAllProduct(){
+    this.customerService.allProduct().subscribe(data=>{
+      this.all_products = data;
+      console.log(this.all_products)
+    },error=>{
+      console.log("My error", error)
+    })
+  }
+
+  buyProduct(id:number){
+    this.show_Checkout = true;
+    this.customerService.quickBuyProduct(id);
+    this.router.navigateByUrl('/checkout');
+  }
+  addToCart(){
+    alert("This is showcase")
+  }
 }
